a434a3e316cabc7b73d0442b8d4088415f016bbe
singjova <65683264+singjova@users.noreply.github.com>
2020-06-03 15:52:17 -0400

    Update newJavaFile.java

--- src/main/java/newJavaFile.java
+++ src/main/java/newJavaFile.java
@@ -16,5 +16,6 @@ public class newJavaFile {
     public static void main(String args[]) {
         System.out.println("jatt");// TODO code application logic here
     System.out.println("yoooo");
+        //jovan
     }
 }
